2ea0ca2fc1d8621d8648f5c49cd407d6f7ce567b
mp-web3 <mp.web3@gmail.com>
2023-04-03 10:23:53 +0200

    improved footer

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
+const arrowButtonUp = document.getElementById('footer_button_arrow');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
@@ -12,3 +13,8 @@ document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', 
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
 }))
+
+arrowButtonUp.addEventListener('click', () => {
+    window.scrollTo({top: 0, behavior: "smooth"});
+} )
+
